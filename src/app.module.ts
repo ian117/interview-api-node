1f49684644b24ac1804ab2fdede9c8bd0278f1e0
Ian Vassallo <spideramerica71@gmail.com>
2024-01-07 18:04:31 -0600

    feat: configModule added to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'test', 'production')
+          .default('development')
+          .required(),
+        PORT: Joi.number().required(),
+        DATABASE_DEVELOPMENT: Joi.alternatives().conditional('NODE_ENV', {
+          is: 'development',
+          then: Joi.string().required(),
+          otherwise: Joi.optional(),
+        }),
+        DATABASE_TEST: Joi.alternatives().conditional('NODE_ENV', {
+          is: 'test',
+          then: Joi.string().required(),
+          otherwise: Joi.optional(),
+        }),
+        DATABASE_URL: Joi.alternatives().conditional('NODE_ENV', {
+          is: 'production',
+          then: Joi.string().required(),
+          otherwise: Joi.optional(),
+        }),
+        SECRET_TOKEN: Joi.string().required(),
+        DOCS_PASSWORD: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
