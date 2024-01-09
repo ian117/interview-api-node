9f46d08dd038218fe611f5079f8e6f000f98e8cc
Ian Vassallo <spideramerica71@gmail.com>
2024-01-09 11:46:40 -0600

    feat: Auth Service methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Sequelize } from 'sequelize-typescript';
+import bcrypt from 'bcryptjs';
 
 import { Users } from '../core/models/users.model';
 
@@ -12,31 +18,64 @@ export class AuthService {
     private readonly sequelize: Sequelize,
   ) {}
 
-  async getOrCreate(email: string | undefined): Promise<Users> {
-    if (email === undefined) {
-      throw new InternalServerErrorException('Not email provided in service');
+  async login(email: string, password: string): Promise<Users> {
+    const user = await this.findUserOr401(email);
+
+    const correctCredentials = this.comparePassword(password, user.password);
+
+    if (!correctCredentials) {
+      throw new UnauthorizedException(`Credentials Don't Match`);
     }
 
-    let user = await this.userModel.findOne({ where: { email } });
+    delete user.password;
+    return user;
+  }
+
+  private async findUserOr401(email): Promise<Users> {
+    const user = await this.userModel.findOne({
+      where: { email },
+      attributes: ['id', 'email', 'password', 'first_name', 'last_name'],
+    });
 
     if (!user) {
-      const transaction = await this.sequelize.transaction();
-      try {
-        user = await this.userModel.create(
-          {
-            first_name: '',
-            last_name: '',
-            email: email.toLocaleLowerCase(),
-            password: '',
-          },
-          { transaction },
-        );
-        await transaction.commit();
-      } catch (error) {
-        await transaction.rollback();
-        throw error;
-      }
+      throw new UnauthorizedException(`User doesn't exist`);
     }
+
     return user;
+  }
+
+  private comparePassword(passwordProvided, passwordHashed): boolean {
+    return bcrypt.compareSync(passwordProvided, passwordHashed);
+  }
+
+  async signUp(userObject): Promise<Users> {
+    await this.ifUserExistbyEmailThrow409(userObject.email);
+
+    const transaction = await this.sequelize.transaction();
+    let user;
+    try {
+      userObject.password = this.hashPassword(userObject.password);
+      user = await this.userModel.create(userObject, { transaction });
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+
+    return user;
+  }
+
+  private async ifUserExistbyEmailThrow409(email): Promise<void> {
+    const user = await this.userModel.findOne({
+      where: { email },
+    });
+
+    if (user) {
+      throw new ConflictException('User already exist');
+    }
+  }
+
+  private hashPassword(password): string {
+    return bcrypt.hashSync(password, 10);
   }
 }
