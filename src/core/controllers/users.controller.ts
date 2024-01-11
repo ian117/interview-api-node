e6ebc330de569946967fdcd43ad30190df5064c0
Ian Vassallo <spideramerica71@gmail.com>
2024-01-11 11:04:59 -0600

    feat: add balance and return User's investments paginated endpoints

--- src/core/controllers/users.controller.ts
+++ src/core/controllers/users.controller.ts
@@ -15,6 +15,7 @@ import { UsersService } from '../services/users.service';
 import { requestUser } from 'src/common/interfaces/common.interface';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUserMe } from '../responses-types/user.responses';
+import { getPagination, getPagingData } from '../helpers/paginations';
 
 @ApiTags('Users')
 @Controller('users')
@@ -36,9 +37,33 @@ export class UsersController {
 
   @UseGuards(AuthGuard('jwt-token'))
   @Get('me/investments')
-  myInvestments(@Query() paginationQueryDto: PaginationQueryDTO) {}
+  async myInvestments(
+    @Req() request: requestUser,
+    @Query() query: PaginationQueryDTO,
+  ) {
+    const { page, size } = query;
+    const { limit, offset } = getPagination(page, size);
+    query.limit = limit;
+    query.offset = offset;
+    const opportunities = await this.usersService.userBalances(
+      request.user.id,
+      query,
+    );
+    const results = getPagingData(opportunities, page, limit);
+    return results;
+  }
 
+  @ApiOperation({
+    summary: 'Add 1,000 MXN to your account',
+    description: 'Add 1,000 MXN to your Wallet',
+  })
   @UseGuards(AuthGuard('jwt-token'))
   @Post('/balance')
-  addBalance() {}
+  async addBalance(@Req() request: requestUser) {
+    await this.usersService.addMoneyToBalance(request.user.id, 1000);
+    return {
+      message:
+        'Se procesado el movimiento en Stripe, y se han agregado 1,000 MXN a su cuenta',
+    };
+  }
 }
--- src/core/services/users.service.ts
+++ src/core/services/users.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Users } from '../models/users.model';
 import { Sequelize } from 'sequelize-typescript';
 import { Wallets } from '../models/wallets.model';
+import { Opportunities } from '../models/opportunities.model';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(Users)
     private readonly userModel: typeof Users,
+    @InjectModel(Opportunities)
+    private readonly opportunityModel: typeof Opportunities,
+    @InjectModel(Wallets)
+    private readonly walletModel: typeof Wallets,
     private readonly sequelize: Sequelize,
   ) {}
 
@@ -23,5 +28,44 @@ export class UsersService {
       ],
     });
     return user;
+  }
+
+  async userBalances(
+    id: string,
+    query,
+  ): Promise<{ rows: Opportunities[]; count: number }> {
+    const options: any = {
+      where: {},
+    };
+
+    const { limit, offset } = query;
+    if (limit && offset) {
+      options.limit = limit;
+      options.offset = offset;
+    }
+
+    options.paranoid = false;
+
+    options.distinct = true;
+
+    options.include = [
+      {
+        model: Users,
+        required: false,
+        where: { id },
+      },
+    ];
+
+    const opportunities = await this.opportunityModel.findAndCountAll(options);
+    return opportunities;
+  }
+
+  async addMoneyToBalance(user_id, amount) {
+    const wallet = await this.walletModel.findOne({
+      where: { user_id },
+    });
+    if (!wallet) throw new NotFoundException('Wallet from user doesnt exist');
+    wallet.balance = Number(wallet.balance) + Number(amount);
+    wallet.save();
   }
 }
