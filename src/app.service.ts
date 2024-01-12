4c7957c9b86373a076650d4f57ca922b91c14c66
Ian Vassallo <spideramerica71@gmail.com>
2024-01-12 00:43:19 -0600

    fix: message on API root endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'interview api ian v';
+    return 'interview-api-ian-v. version@1.0';
   }
 }
